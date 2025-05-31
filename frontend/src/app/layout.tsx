3bbf812d262e2d407c66bd04da1203e699b70d56
Andrew Dutka <andrewadutka@gmail.com>
2025-05-30 21:13:52 -0300

    made default html dark layout to fix scroll bg

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,18 +2,21 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-	title: "Syllabusy",
-	description: "",
+    title: "Syllabusy",
+    description: "",
 };
 
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" style={{ backgroundColor: "#060606" }}>
-			<body>{children}</body>
-		</html>
-	);
+    return (
+        <html
+            lang="en"
+            style={{ colorScheme: "dark", backgroundColor: "#060606" }}
+        >
+            <body>{children}</body>
+        </html>
+    );
 }
